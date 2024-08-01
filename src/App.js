88328e34479566e30bb5d3c160c3cfcbec4f9fd9
Kiarasinghh <156370071+Kiarasinghh@users.noreply.github.com>
2024-08-01 18:52:05 +0800

    uploading to git hub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Countdown from "./Countdown";
+import "./index.css";
+import moment from 'moment';
+
+
 
 function App() {
+  const[data,setData]=useState({})
+  const[location,setLocation]=useState('')
+  const[lat,setLat]=useState(0)
+  const[long,setLong]=useState(0)
+  const[UV,setUV]=useState([])
+  const UVapikey="openuv-dln7c1rlyr3t1yw-io";
+  const UVapiurl="https://api.openuv.io/api/v1/uv";
+
+  
+  const url2=`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=uv_index_max,uv_index_clear_sky_max&timezone=auto&forecast_days=1`
+  const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=cd990b4f0752b81744e02484bddfa385&units=metric`
+
+  function SPF(){
+    if(UV.result){
+      if((UV.result.uv)>=3){
+        return "YES!"
+      }else{
+        return "Not needed"
+      }
+    }
+  }
+
+  function getTime(time){
+    return(moment(time).format("h:mm a"))
+  }
+  
+  //get coordinates
+  const searchLocation=(event)=>{
+    if(event.key==='Enter'){
+      axios.get(url).then((response)=>{
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+  useEffect(()=>{
+    if(data&&data.coord){
+      setLat(data.coord.lat);
+      setLong(data.coord.lon);
+    }
+  },[data]);
+
+
+  useEffect(()=>{
+    if(lat&&long){
+      axios.get(`https://api.openuv.io/api/v1/uv?lat=${lat}&lng=${long}`, {
+        headers: {
+          'x-access-token': 'openuv-dln7c1rlyr3t1yw-io',
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((result) => {
+        setUV(result.data)
+      })
+      .catch((err) => console.error(err));
+    }
+  },[lat,long]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <button onClick={getTime}>click me</button>
+    </div>
+
+      <div className="search">
+        <input
+        value={location}
+        onChange={event=>setLocation(event.target.value)}
+        onKeyDown={searchLocation}
+        placeholder='Enter location'
+        type="text"
+        />
+      </div>
+
+      <div className="container">
+        <div className="top">
+
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+
+          <div className="UV">
+          {UV.result ? <h1>UV INDEX: {UV.result.uv}</h1> : null}
+          </div>
+
+          <div className="reminder">
+            <Countdown/>
+          </div>
+
+          <div className="description">
+          {data.weather ? <p>{data.weather[0].main}</p>:null}
+          </div>
+
+        </div>
+        <div className="bottom">
+
+          <div className="temp">
+            {data.main ? <p>{data.main.temp}C</p>:null}
+            <p>Temperature</p>
+          </div>
+
+          <div className="maxUV">
+          {UV.result ? <p>{UV.result.uv_max} at {getTime(UV.result.uv_max_time)}</p>:null}
+            <p>Max UV</p>
+          </div>
+
+          <div className="SPF Reccomended?">
+          <p> {SPF()}</p>
+            <p>SPF Reccomendation</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Countdown.js
@@ -0,0 +1,87 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Alert, Button, Toast, ToastBody, ToastHeader, ToastContainer}from 'react-bootstrap'
+import "./countdown.css";
+
+function Countdown () {
+    const [applied,setApplied]=useState(false);
+    const [hours,setHours]=useState(0);
+    const [minutes,setMinutes]=useState(0);
+    const[seconds,setSeconds]=useState(5);
+    const[show,setShow]=useState(false);
+
+    useEffect(()=>{
+        let interval;
+        if(applied){
+            interval=setInterval(()=>{
+                if(seconds>0){
+                    setSeconds(s=>s-1)
+                }else if(minutes>0){
+                    setMinutes(m=>m-1)
+                    setSeconds(59)
+                }else if(hours>0){
+                    setHours(h=>h-1)
+                    setMinutes(59)
+                    setSeconds(59)
+                }else{
+                    setShow(true)
+                }
+            },1000)  
+        }
+
+        if(seconds===0 && minutes===0 && hours===0){
+            setShow(true)
+        }
+
+        return ()=>clearInterval(interval);
+
+    },[seconds,minutes,hours,applied]);
+
+
+    function reset(){
+        setApplied(false)
+        setShow(false)
+        setHours(0)
+        setMinutes(0)
+        setSeconds(5)
+    }
+
+  return (
+    <div className='center' >
+
+        <div className='countdown'>
+            <h1>{hours}:{minutes}:{seconds}</h1>
+        </div>
+
+        <div className='applyButton'>
+            <Button variant="success" onClick={!applied ? ()=>setApplied(true) : null} >  {!applied ? "Apply" :"Have fun in the sun!"}</Button>
+        </div>
+
+        <div className='resetButton'>
+                <Button onClick={reset}>Reset</Button>
+        </div>
+
+        <div className='notification'>
+            {show&&   <Alert variant="danger" onClose={()=>setShow(false)} dismissible> <Alert.Heading>Time to reapply</Alert.Heading> </Alert>}
+              
+        </div>
+
+        <ToastContainer className="notification" position="top-end">
+            <Toast>
+                <ToastHeader>
+                            Reapplication reminder!
+                </ToastHeader>
+                <ToastBody className="Dark">
+                            It's been two hours, time to reapply!
+                </ToastBody>
+            </Toast>
+        </ToastContainer>
+
+    </div>
+
+   
+  )
+}
+
+export default Countdown
